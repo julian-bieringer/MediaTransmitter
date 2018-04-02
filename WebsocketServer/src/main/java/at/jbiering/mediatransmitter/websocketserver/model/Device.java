f8c881bbc91f63a3c36e6e07ceb6ec8ac5393757
julian-bieringer <bieringer.julian@gmail.com>
2018-04-02 14:49:05 +0200

    added new fields such as os version and type

--- WebsocketServer/src/main/java/at/jbiering/mediatransmitter/websocketserver/model/Device.java
+++ WebsocketServer/src/main/java/at/jbiering/mediatransmitter/websocketserver/model/Device.java
@@ -6,55 +6,76 @@ public class Device {
     private String name;
     private String status;
     private String type;
-    private String description;
+    private String modelDescription;
+    private String osType;
+    private String osVersion;
 
     public Device() {
     }
-    
-    public Device(String name, String status, String type, String description) {
+
+	public Device(String name, String status, String type, String modelDescription, String osType, String osVersion) {
+		super();
 		this.name = name;
 		this.status = status;
 		this.type = type;
-		this.description = description;
+		this.modelDescription = modelDescription;
+		this.osType = osType;
+		this.osVersion = osVersion;
 	}
 
 	public long getId() {
-        return id;
-    }
-    
-    public String getName() {
-        return name;
-    }
+		return id;
+	}
 
-    public String getStatus() {
-        return status;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getType() {
-        return type;
-    }
-    
-    public String getDescription() {
-        return description;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+	public String getStatus() {
+		return status;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
-    
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModelDescription() {
+		return modelDescription;
+	}
+
+	public void setModelDescription(String modelDescription) {
+		this.modelDescription = modelDescription;
+	}
+
+	public String getOsType() {
+		return osType;
+	}
+
+	public void setOsType(String osType) {
+		this.osType = osType;
+	}
+
+	public String getOsVersion() {
+		return osVersion;
+	}
+
+	public void setOsVersion(String osVersion) {
+		this.osVersion = osVersion;
+	}
 }
