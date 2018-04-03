4605ac3c07b66fac308647e813a4e26a27375384
julian-bieringer <bieringer.julian@gmail.com>
2018-04-03 15:36:23 +0200

    added new model media file for handling different types of files

---
+++ WebsocketServer/src/main/java/at/jbiering/mediatransmitter/websocketserver/model/MediaFile.java
@@ -0,0 +1,38 @@
+package at.jbiering.mediatransmitter.websocketserver.model;
+
+public class MediaFile {
+	
+	private String bytesBase64;
+	private String fileExtension;
+	private String fileName;
+	
+	public MediaFile(String bytesBase64, String fileExtension, String fileName) {
+		this.bytesBase64 = bytesBase64;
+		this.fileExtension = fileExtension;
+		this.fileName = fileName;
+	}
+
+	public String getBytesBase64() {
+		return bytesBase64;
+	}
+
+	public void setBytesBase64(String bytesBase64) {
+		this.bytesBase64 = bytesBase64;
+	}
+
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public void setFileExtension(String fileExtension) {
+		this.fileExtension = fileExtension;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+}
