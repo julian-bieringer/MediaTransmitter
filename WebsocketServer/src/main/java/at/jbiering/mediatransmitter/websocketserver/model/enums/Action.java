53c363a79042e391d7905e2639f05da524bcc84b
julian-bieringer <bieringer.julian@gmail.com>
2018-04-03 15:35:12 +0200

    moved enums to different package which meets the structure better

---
+++ WebsocketServer/src/main/java/at/jbiering/mediatransmitter/websocketserver/model/enums/Action.java
@@ -0,0 +1,6 @@
+package at.jbiering.mediatransmitter.websocketserver.model.enums;
+
+public enum Action
+{
+	ADD, REMOVE, TOGGLE, RETRIEVE_SUBSCRIBERS, SUBSCRIBER_LIST_UPDATE_REQUIRED, SEND_FILE, RETRIEVE_FILE
+}
--- WebsocketServer/src/main/java/at/jbiering/mediatransmitter/websocketserver/websocket/enums/Action.java
+++
@@ -1,6 +0,0 @@
-package at.jbiering.mediatransmitter.websocketserver.websocket.enums;
-
-public enum Action
-{
-	ADD, REMOVE, TOGGLE, RETRIEVE_SUBSCRIBERS, SUBSCRIBER_LIST_UPDATE_REQUIRED
-}
