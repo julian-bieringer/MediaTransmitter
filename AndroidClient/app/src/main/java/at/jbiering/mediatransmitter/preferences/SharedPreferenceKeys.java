d897d1428418d2b28179fd20853930bc562057cb
julian-bieringer <bieringer.julian@gmail.com>
2018-04-07 16:39:17 +0200

    grouped all preference key constants to a separate class

---
+++ AndroidClient/app/src/main/java/at/jbiering/mediatransmitter/preferences/SharedPreferenceKeys.java
@@ -0,0 +1,13 @@
+package at.jbiering.mediatransmitter.preferences;
+
+public class SharedPreferenceKeys {
+
+    //user preference keys
+    public static final String USER_NAME_KEY = "USER_NAME";
+
+    //server preference keys
+    public static final String SERVER_IP_KEY = "SERVER_IP";
+    public static final String SERVER_PORT_KEY = "SERVER_PORT";
+    public static final String WEBSOCKET_CONTEXT_PATH_KEY = "WS_CONTEXT_PATH";
+
+}
